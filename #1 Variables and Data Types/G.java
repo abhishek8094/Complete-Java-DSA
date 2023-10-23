82e887b0d011ae7d3a28f649331cad79bbd09608
Abhishek Singh <85206759+abhishek8094@users.noreply.github.com>
2023-10-23 10:13:18 +0530

    Add files via upload

---
+++ #1 Variables and Data Types/G.java
@@ -0,0 +1,13 @@
+//Sum of a and b By user input method
+import java.util.*;
+public class G{
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println(sum);
+
+    }
+}
---
+++ #1 Variables and Data Types/H.java
@@ -0,0 +1,14 @@
+//Product of a and b by user input
+
+import java.util.*;
+public class H{
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+
+        int Product=a*b;
+        System.out.println(Product);
+
+    }
+}
